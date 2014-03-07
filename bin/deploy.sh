7df5f40f3755dd32ed328cc0c8dbb7a9bfb827ab
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2014-03-07 13:18:56 +0000

    Reorganize bins

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,24 +1,6 @@
-#!/bin/zsh
+#!/bin/env bash
 
+# Capistrano/mina replacement
 
-#Use example:
-#In this ansible directory, type:
-# bin/deploy.sh ubuntu staging -t role_nginx
-# ubuntu = remote_user
-# staging = deploy_to_enviroment
-# -t role_nginx is any ansible parameter (this is optional, instead execute normal playbook)
-
-#default remote_user is root
-remote_user="$1"
-
-#Inventory - production | staging
-deploy_to_environment="$2"
-
-#Remove parameter 1 and parameter 2 to attend ansible extra parameters via cli
-shift 2
-
-ansible-playbook \
-	playbooks/simplevps.yml \
-	-i inventory/${deploy_to_environment} \
-	--extra-vars "remote_user=${remote_user}" \
-	$*
+# deplpy.sh [env]
+# deploy.sh staging
