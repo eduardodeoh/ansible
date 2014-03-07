d5e0ed5a99d072c85d424e0a9844c13d44f28f0b
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2014-03-07 01:28:34 -0300

    Changes in ansible strucuture
    Changes to permit deploy for any environment and directories
    organization

---
+++ bin/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+
+#Use example:
+#In this ansible directory, type:
+# bin/deploy.sh ubuntu staging -t role_nginx
+# ubuntu = remote_user
+# staging = deploy_to_enviroment
+# -t role_nginx is any ansible parameter (this is optional, instead execute normal playbook)
+
+#default remote_user is root
+remote_user="$1"
+
+#Inventory - production | staging
+deploy_to_environment="$2"
+
+#Remove parameter 1 and parameter 2 to attend ansible extra parameters via cli
+shift 2
+
+ansible-playbook \
+	playbooks/simplevps.yml \
+	-i inventory/${deploy_to_environment} \
+	--extra-vars "remote_user=${remote_user}" \
+	$*
