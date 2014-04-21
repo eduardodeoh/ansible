b9dd86f5b72503ced773ff60233d5e3d9c5530a8
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2014-04-21 17:33:36 -0300

    Added fix to install ruby 2.1.1 on Ubuntu 14.04

--- roles/ruby/files/ruby.sh
+++ roles/ruby/files/ruby.sh
@@ -7,8 +7,16 @@ if [ -d "${RBENV_ROOT}" ]; then
   eval "$(rbenv init -)"
 fi
 
+#Rbenv patch to install Ruby 2.x on Ubuntu 14.04
+#https://bugs.ruby-lang.org/issues/9578
+#https://github.com/sstephenson/ruby-build/issues/526
+if [ `lsb_release -c | awk '{print $2}'` == "trusty" ]
+then
+  curl -fsSL https://gist.github.com/mislav/a18b9d7f0dc5b9efc162.txt | rbenv install --patch 2.1.1 --force --verbose
+else
+  rbenv install $1 --force --verbose
+fi
 
-rbenv install $1 --force --verbose
 rbenv global $1
 
 #BUG - http://bugs.ruby-lang.org/issues/5060
